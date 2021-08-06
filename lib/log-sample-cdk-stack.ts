c620efe5590f3d245a24e941bf9a0c1576f9b794
SpringMT <today.is.sky.blue.sky@gmail.com>
2021-08-06 23:43:31 +0900

    feat: Fargate with log-sample

--- lib/log-sample-cdk-stack.ts
+++ lib/log-sample-cdk-stack.ts
@@ -1,9 +1,84 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core"
+import * as ec2 from "@aws-cdk/aws-ec2"
+import { ApplicationLoadBalancedFargateService } from "@aws-cdk/aws-ecs-patterns"
+import * as iam from '@aws-cdk/aws-iam'
+import * as logs from '@aws-cdk/aws-logs'
+import * as ecs from "@aws-cdk/aws-ecs"
+import { ApplicationProtocol } from "@aws-cdk/aws-elasticloadbalancingv2"
+import { RemovalPolicy } from "@aws-cdk/core"
 
 export class LogSampleCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    // Create VPC
+    const vpc = new ec2.Vpc(this, 'log-sample-vpc', { maxAzs: 2});
 
-    // The code that defines your stack goes here
+    // Create ECS Cluster
+    const cluster = new ecs.Cluster(this, 'log-sample-ecs', {
+      vpc,
+    })
+
+    // IAM Role
+    const executionRole = new iam.Role(this, 'LogSampleEcsTaskExecutionRole', {
+      roleName: 'log-sample-ecs-task-execution-role',
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('CloudWatchAgentServerPolicy'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonECSTaskExecutionRolePolicy'),
+      ],
+    })
+    executionRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: [
+          'firehose:PutRecord',
+          'firehose:PutRecordBatch',
+        ],
+        resources: ['*'],
+      })
+    )
+
+    const serviceTaskRole = new iam.Role(this, 'LogSampleEcsServiceTaskRole', {
+      roleName: 'log-sample-ecs-service-task-role',
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    })
+
+    // https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/task_definition_parameters.html
+    const taskDef = new ecs.FargateTaskDefinition(this, "LogSampleTaskDefinition", {
+      family: 'log-sample',
+      memoryLimitMiB: 1024,
+      cpu: 512,
+      executionRole: executionRole,
+      taskRole: serviceTaskRole,
+    })
+    const logGroup = new logs.LogGroup(this, 'LogSampleLogGroup', {
+       logGroupName: '/ecs/log-sample-log',
+       removalPolicy: RemovalPolicy.DESTROY // 今回は消す設定にする
+    })
+    const containerDef = taskDef.addContainer('LogSampleContainerDefinition',{
+      image: ecs.ContainerImage.fromRegistry("springmt/log-sample:v0.0.2"),
+      cpu: 256,
+      memoryLimitMiB: 256,
+      logging: ecs.LogDriver.awsLogs({
+        streamPrefix: 'log-sample',
+        logGroup,
+      }),
+    })
+  
+    containerDef.addPortMappings({
+      containerPort: 8080
+    })
+
+    const service = new ApplicationLoadBalancedFargateService(this, 'LogSampleFargateService', {
+      serviceName: 'log-sample-fargate-service',
+      cluster,
+      taskDefinition: taskDef,
+      desiredCount: 1,
+      maxHealthyPercent: 200,
+      minHealthyPercent: 50,
+    })
+    
+    new cdk.CfnOutput(this, "LoadBalancerDNS", {
+      value: service.loadBalancer.loadBalancerDnsName
+    })
   }
 }
